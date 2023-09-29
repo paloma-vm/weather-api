62fc9131a8ee37351d7c33da130d88a2f769481d
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-09-28 23:56:20 -0700

    getWeather working again

--- weather.js
+++ weather.js
@@ -1,8 +1,23 @@
-function getWeather(apiKey, zip, units, onSuccess, onError) {
-    // Replace this with your own API key!
+async function getWeather(zip, apiKey, units = 'imperial') {
     
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-    fetch(path)
+    try {
+      // try something
+      const res = await fetch(path)
+      const json = await res.json()
+      return json
+    } catch(err) {
+      // handle errors here
+      return err
+    }
+    
+    
+}
+
+async function getWeatherByZip(apiKey, zip, units, onSuccess, onError) {
+    
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+    await fetch(path)
       .then(res => res.json())
       .then(json => {
         onSuccess(json)
@@ -10,4 +25,30 @@ function getWeather(apiKey, zip, units, onSuccess, onError) {
       .catch(err => onError(err))
 }
 
-export default getWeather
+async function getWeatherByCity(apiKey, city, units, onSuccess, onError) {
+    
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${city}&appid=${apiKey}&units=${units}`
+    await fetch(path)
+      .then(res => res.json())
+      .then(json => {
+        onSuccess(json)
+      })
+      .catch(err => onError(err))
+}
+// an option maybe
+async function getWeatherByZoneType(apiKey, zoneType, units, onSuccess, onError) {
+    
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+    await fetch(path)
+      .then(res => res.json())
+      .then(json => {
+        onSuccess(json)
+      })
+      .catch(err => onError(err))
+}
+
+// module system
+//export to make available somewhere else
+export { 
+  getWeather 
+}
